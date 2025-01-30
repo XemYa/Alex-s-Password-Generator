c84a8fedf7d3c639eae38804ee6983ec6c040764
Alex <126931036+XemYa@users.noreply.github.com>
2025-01-29 22:11:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ print("Alex's Password Generator")
 response=input("Would you like a Password Generated? yes/no")
 
 if response == "yes":
-    password = ''.join(random.choices(string.ascii_letters+string.digits,k=10))
-    
-    print(f"Password: {password}")
-    
+    passwords = []
+    for _ in range(5):
+        password = ''.join(random.choices(string.ascii_letters + string.digits,k=10))
+        passwords.append(password)
+
+    print("Generated passwords:")
+    print(passwords)
 else:
     print("Have a good day!")
